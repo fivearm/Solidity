020aba3b37a0e062fa51a8ed479c6992127e21da
fivearm <fivearm@gmail.com>
2022-11-29 12:51:20 -0500

    ExampleStruct

---
+++ contracts/ExampleStruct.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier:  MIT
+
+pragma solidity 0.8.17;
+
+contract PaymentReceived{
+
+    address public from;
+    uint public amount;
+
+    constructor(address _from, uint _amount) {
+        from = _from;
+        amount = _amount;
+    }
+
+}
+
+contract Wallet{
+
+    PaymentReceived public payment;
+
+    function payContract() public payable {
+        payment = new PaymentReceived(msg.sender, msg.value);
+    }
+}
+
+contract Wallet2{
+
+    struct PaymentReceivedStruct {
+        address from;
+        uint amount;
+    }
+
+    PaymentReceivedStruct public payment;
+
+    function payContract() public payable {
+        payment = PaymentReceivedStruct(msg.sender, msg.value);
+    }
+}
+
